87719f0d9cddb00a4567f9cc825eb38bad396706
Juan Salvatore <juansalvatore@live.com.ar>
2017-11-03 11:37:06 -0300

    Create shared text editor and reactive session date

---
+++ textcircle.js
@@ -0,0 +1,38 @@
+this.Documents = new Mongo.Collection("documents");
+
+if (Meteor.isClient){
+
+
+	// update the session every 1000ms
+	Meteor.setInterval(function(){
+		Session.set("current_date", new Date());
+	}, 1000);
+
+	Template.date_display.helpers({
+		current_date: function(){
+			return Session.get("current_date");
+		}
+	});
+
+	Template.editor.helpers({
+		docid:function(){
+			console.log('Doc id helper:');
+			console.log(Documents.findOne());
+			var doc = Documents.findOne();
+			if (doc) {
+				return doc._id;
+			} else {
+				return undefined;
+			}
+		}
+	});
+}
+
+if (Meteor.isServer){
+	Meteor.startup(function(){
+		// code to run on server at startup
+		if (!Documents.findOne()) { // no documents yet!
+			Documents.insert({title: "my new document"});
+		}
+	})
+}
