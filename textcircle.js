cebc09e4360cc85bb4f8f1d1bf9b0a3b64d089d5
Juan Salvatore <juansalvatore@live.com.ar>
2017-11-03 12:34:35 -0300

    Add jquery to read editor

--- textcircle.js
+++ textcircle.js
@@ -29,6 +29,7 @@ if (Meteor.isClient){
 			return function(editor) {
 				editor.on("change", function(cm_editor, info){
 					console.log(cm_editor.getValue());
+					$("#viewer_iframe").contents().find("html").html(cm_editor.getValue());
 				});
 			}
 		}
